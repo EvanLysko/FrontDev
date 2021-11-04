69cec26d8e1b56ad9e4da48ea51357fc6cea7360
Evan Lysko <64044993+EvanLysko@users.noreply.github.com>
2021-11-04 16:25:38 -0400

    Made main styling layout of website

--- WeatherPage/scripts.js
+++ WeatherPage/scripts.js
@@ -1,13 +1,45 @@
 "use strict"
-alert("Hello World")
 
-/**
- * Returns x raised to the n-th power.
- *
- * @param {number} x The number to raise.
- * @param {number} n The power, must be a natural number.
- * @return {number} x raised to the n-th power.
- */
-function pow(x, n) {
-  return x**n;
+/*
+
+get input automatically if possible then if not have an input field for city/zip
+metric/american
+
+current weather information
+hourly for 2 days
+daily for 7 days
+air pollution API and link to information
+national weather alert banner
+weather map
+
+include dynamic features that depend on weather data maybe clothing items depending on temp, umbrella depending on rain
+
+try to make it so that the page doesn't require scrolling and is as useful as possible
+*/
+styling();
+window.addEventListener("resize", styling, false);
+
+function styling() {
+//fix styling
+let width = document.body.clientWidth;
+let height = document.body.clientHeight;
+
+console.log(width);
+document.getElementById("main").style.width = width;
+document.getElementById("main").style.height = height;
+
+//height = height - the 300px for map and current
+height -= 300;
+
+//width = width - the 300px for map --- to dynamically set width of 
+width -= 305;
+console.log(width);
+document.getElementById("current").style.width = width.toString()+"px";
+
+document.getElementById("header").style.height = Math.floor(height*.12).toString()+"px";
+document.getElementById("weatheralert").style.height = Math.floor(height*.06).toString()+"px";
+document.getElementById("search").style.height = Math.floor(height*.08).toString()+"px";
+document.getElementById("hourly").style.height = Math.floor(height*.32).toString()+"px";
+document.getElementById("daily").style.height = Math.floor(height*.32).toString()+"px";
+document.getElementById("footer").style.height = Math.floor(height*.08).toString()+"px";
 }
