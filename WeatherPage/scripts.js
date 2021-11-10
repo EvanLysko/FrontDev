7c34508651d97d6d3ab97ee98799ef8dc9af5830
Evan Lysko <64044993+EvanLysko@users.noreply.github.com>
2021-11-09 23:10:32 -0500

    Logo and inputs
    added logos and styling for input and button to toggle F/C. need to make them actually do things and use API

--- WeatherPage/scripts.js
+++ WeatherPage/scripts.js
@@ -20,6 +20,7 @@ styling();
 window.addEventListener("resize", styling, false);
 
 function styling() {
+
 //fix styling
 let width = document.body.clientWidth;
 let height = document.body.clientHeight;
@@ -34,8 +35,35 @@ document.getElementById("current").style.width = width.toString()+"px";
 
 document.getElementById("header").style.height = Math.floor(height*.12).toString()+"px";
 document.getElementById("weatheralert").style.height = Math.floor(height*.06).toString()+"px";
-document.getElementById("search").style.height = Math.floor(height*.08).toString()+"px";
+let search = Math.floor(height*.08);
+document.getElementById("search").style.height = search.toString()+"px";
 document.getElementById("hourly").style.height = Math.floor(height*.32).toString()+"px";
 document.getElementById("daily").style.height = Math.floor(height*.32).toString()+"px";
 document.getElementById("footer").style.height = Math.floor(height*.08).toString()+"px";
+
+
+document.getElementById("location").style.height = (search-8).toString()+"px";
+document.getElementById("location").style.margin = (Math.floor((search-8)/5)).toString()+"px";
+
+document.getElementById("slider").style.height = (search-8).toString()+"px";
+document.getElementById("slider").style.margin = (Math.floor((search-8)/5)).toString()+"px";
+
+// need slider::after top
+let afterTop = document.head.appendChild(document.createElement("style"));
+let topcalc = Math.floor((search-8)/8).toString();
+let topstring = ".slider:after {top: " + topcalc + "px;}";
+afterTop.innerHTML = topstring;
+
+//slider:before height and width
+let beforestyle = document.head.appendChild(document.createElement("style"));
+let sizecalc =  Math.floor(search-16).toString();
+let beforestring = ".slider:before {height: " + sizecalc + "px;}";
+beforestyle.innerHTML = beforestring;
+
+//input:checked + .slider:after top
+let inputstyle = document.head.appendChild(document.createElement("style"));
+inputstyle.innerHTML = "input:checked + .slider:after {top:" + topcalc + "px;}";
+
 }
+
+//API KEY : 534b29ce483966111fb1ddf737054542
