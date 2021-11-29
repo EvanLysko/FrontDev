33dbd0d2c87f6da18f92444f80d513112b5c9974
Evan Lysko <64044993+EvanLysko@users.noreply.github.com>
2021-11-29 12:02:38 -0500

    location, units  and current working

--- WeatherPage/scripts.js
+++ WeatherPage/scripts.js
@@ -18,6 +18,7 @@ try to make it so that the page doesn't require scrolling and is as useful as po
 */
 styling();
 window.addEventListener("resize", styling, false);
+document.getElementById("checkbox").addEventListener("click", getWeather, false);
 
 function styling() {
 
@@ -77,17 +78,19 @@ let current2nd = "&appid=";
 
 function getByZip(zip) {//make query string with zip code
   let starter = "zip=";
-  let queryString = current1st + starter + zip + current2nd + APIKey;
+  let units = getUnits();
+  let queryString = current1st + starter + zip + units + current2nd + APIKey;
   return queryString; 
 }
 
 function getByCity(city) {//make query string with city
   let starter = "q=";
-  let queryString = current1st + starter + city + current2nd + APIKey;
+  let units = getUnits();
+  let queryString = current1st + starter + city + units + current2nd + APIKey;
   return queryString;    
 }
 
-function getCurrent() {// get current weather
+function getWeather() {// get current weather
   //get input
   let input = document.getElementById("location").value;
   console.log(input);
@@ -106,26 +109,115 @@ function getCurrent() {// get current weather
 
   getJSON(queryString, function(err, data) {
     if (err !== null) {//if there's an error code
-      alert('Something went wrong: ' + err);
+      
     } 
     else {
       //this is where we do stuff with data
       //add icon
       //get icon based off of condition codes with other function
-      //clear current
-      document.getElementById("current").innerHTML = "";
-      //put in new info
-      console.log(data);
-      let icon = chooseIcon(data);
-      let weatherIcon = document.createElement("img");
-      weatherIcon.src = icon;
-      weatherIcon.id = "weatherIcon";
-      document.getElementById("current").appendChild(weatherIcon);
+      //clear page
+      clearPage();
+      //load new info
+      getCurrent(data);
+      getHourly(data);
+      getDaily(data);
+      getMap(data);
+      getAlerts(data);
     }
-    console.log("callback");
   });
 
     }
+
+
+function clearPage() {
+  document.getElementById("current").innerHTML = "";
+  document.getElementById("weatheralert").innerHTML = "";
+  document.getElementById("hourly").innerHTML = "";
+  document.getElementById("daily").innerHTML = "";
+  document.getElementById("map").innerHTML = "";
+}
+
+function getUnits() {
+  let units = "&units=";
+  let check = document.getElementById("checkbox").checked;
+
+  if (check == false){//celcius
+    units += "metric";
+  }
+  else {
+    units += "imperial";
+  }
+  return units;
+}
+
+
+function getCurrent(data) {
+  //choose and add icon
+  let icon = chooseIcon(data);
+  let weatherIcon = document.createElement("img");
+  weatherIcon.src = icon;
+  weatherIcon.id = "currentIcon";
+  document.getElementById("current").appendChild(weatherIcon);
+
+  //get weather information
+  console.log(data);
+  let temp = data.main.temp;
+  let feelsLike = data.main.feels_like;
+  let temp_min = data.main.temp_min;
+  let temp_max = data.main.temp_max;
+  let humidity = data.main.humidity;
+  let name = data.name;
+  console.log(temp);
+
+  //put it on the page
+  //header
+  let header = document.createElement("h1");
+  header.innerHTML = "Current Weather in " + name;
+
+  let unit = "&#8451";
+  if (getUnits() == "&units=imperial") {
+    unit = "&#8457";
+  }
+
+  //information
+  let information = document.createElement("p");
+  let info = "Temperature: " + temp + " " + unit + "<br>" +
+  "Feels Like: " + feelsLike + " " + unit + "<br>" +
+  "Min: " + temp_min + " " + unit + "<br>" +
+  "Max: " + temp_max + " " + unit + "<br>" +
+  "Humidity: " + humidity + "%" + "<br>";
+  console.log(info);
+  information.innerHTML = info; 
+
+  let current = document.getElementById("current")
+  current.appendChild(header);
+  current.appendChild(information);
+
+  
+
+}
+
+function getHourly(data) {
+
+
+}
+
+function getDaily(data) {
+
+
+}
+
+function getAlerts(data) {
+
+
+}
+
+function getMap(data) {
+
+
+}
+
+
 
 function getJSON(url, callback){//get json from api
     let xhr = new XMLHttpRequest();
@@ -163,11 +255,21 @@ function chooseIcon(data) {
     console.log(3);
   }
   else if ( temp == 800){//clear
-    icon = "resources/sunny.png";
+    if (data.weather[0].icon == "01d") {
+      icon = "resources/sunny.png";
+    }
+    else {
+      icon = "resources/moony.png";
+    }
     console.log(4);
   }
   else if ( temp == 801){//partly cloudy
-    icon = "resources/partlysunny.png";
+    if (data.weather[0].icon == "02d") {
+      icon = "resources/partlysunny.png";
+    }
+    else {
+      icon = "resources/partlymoony.png";
+    }
     console.log(5);
   }
   else if ( temp > 800){//cloudy
@@ -177,3 +279,5 @@ function chooseIcon(data) {
   console.log(icon);
   return icon;
 }
+
+getUnits();
